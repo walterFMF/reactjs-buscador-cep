084be777bcd2863c3875a676cb5c49786d07dca4
walterFMF <walter_but@hotmail.com>
2024-02-12 13:09:21 -0300

    estilizacao da pagina

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className='containerInput'>
+
+        <input type='text' placeholder='Digite um cep...'/>
+
+        <button className='buttonSearch'>
+          <FiSearch size={25} color="#fff"/>
+        </button>
+
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 74003222</h2>
+        <span>Rua 26</span>        
+        <span>complemento</span>
+        <span>Bairro cardoso </span>
+        <span>Campo Grande </span>
+      </main>
     </div>
   );
 }
